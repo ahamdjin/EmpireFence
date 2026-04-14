20fbce00518a01618629dac1d3119ac84519b4a1
Ahmad <ahamdjin34@gmail.com>
2026-04-15 04:37:52 +0500

    Tighten EmpireFence SEO hygiene

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,38 +1,38 @@
 import { getAllPosts, getAllServices } from "@/lib/content";
-import { business, serviceAreas } from "@/lib/site";
+import { business, contentDates, serviceAreas } from "@/lib/site";
 import { areaPath, areasIndexPath, privacyPolicyPath, servicePath, servicesIndexPath, termsPath } from "@/lib/paths";
 
 export default async function sitemap() {
   const services = await getAllServices();
   const posts = await getAllPosts();
   const staticRoutes = [
-    "",
-    "/about-us",
-    servicesIndexPath,
-    "/gallery",
-    areasIndexPath,
-    "/contact-us",
-    "/blog",
-    privacyPolicyPath,
-    termsPath,
+    { path: "", lastModified: contentDates.site },
+    { path: "/about-us", lastModified: contentDates.site },
+    { path: servicesIndexPath, lastModified: contentDates.services },
+    { path: "/gallery", lastModified: contentDates.site },
+    { path: areasIndexPath, lastModified: contentDates.areas },
+    { path: "/contact-us", lastModified: contentDates.site },
+    { path: "/blog", lastModified: contentDates.site },
+    { path: privacyPolicyPath, lastModified: contentDates.legal },
+    { path: termsPath, lastModified: contentDates.legal },
   ];
 
   return [
-    ...staticRoutes.map((path) => ({
+    ...staticRoutes.map(({ path, lastModified }) => ({
       url: `${business.website.replace(/\/$/, "")}${path || "/"}`,
-      lastModified: new Date(),
+      lastModified: new Date(lastModified),
     })),
     ...services.map((service) => ({
       url: `${business.website.replace(/\/$/, "")}${servicePath(service.slug)}`,
-      lastModified: new Date(),
+      lastModified: new Date(service.data.modifiedDate || contentDates.services),
     })),
     ...serviceAreas.map((area) => ({
       url: `${business.website.replace(/\/$/, "")}${areaPath(area.slug)}`,
-      lastModified: new Date(),
+      lastModified: new Date(area.modifiedDate || contentDates.areas),
     })),
     ...posts.map((post) => ({
       url: `${business.website.replace(/\/$/, "")}/blog/${post.slug}`,
-      lastModified: new Date(post.data.date),
+      lastModified: new Date(post.data.modifiedDate || post.data.date),
     })),
   ];
 }
